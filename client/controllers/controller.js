dd4e6db6aa48fbec52ce76a4cfb8b349413635e7
Ignacio <ignaciopreda@gmail.com>
2017-10-25 16:32:15 -0300

    ABM finalizado, falta retoques interfaz

--- client/controllers/controller.js
+++ client/controllers/controller.js
@@ -13,10 +13,23 @@ myApp.controller('empController', function($scope,$route,$routeParams,$http){
     });
   };
 
-  $scope.editEmployee = function(){
+  $scope.addEmployee = function(){
+    $http.post('/api/employees/', $scope.employee).then(function(response){
+      window.location.href = '/'; //luego de dar el alta te redirige a la lista
+    });
+  };
+
+  $scope.updateEmployee = function(){
     var id = $routeParams.id;
-    $http.get('/api/employees/' + id).then(function(response){
-      $scope.employee = response.data;
+    $http.put('/api/employees/' + id, $scope.employee).then(function(response){
+      window.location.href = '/';
+    });
+  };
+
+  $scope.deleteEmployee = function(id){
+    var id = id;
+    $http.delete('/api/employees/' + id, $scope.employee).then(function(response){
+      $route.reload();
     });
   };
 
